b8c37257c7c22ae3343a3c81476685e7c66f396f
AJ <a.j.woodward53@gmail.com>
2024-12-02 19:34:30 +0000

    added static build in dist

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/anything-immersive-demos',
+  output: 'export',
+  reactStrictMode: true,
+  distDir: 'dist',
+};
 
 export default nextConfig;
