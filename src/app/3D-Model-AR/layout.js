3c7dddd0fd310a43f9e159b0d4b18b51062b1a50
AJ <a.j.woodward53@gmail.com>
2024-12-03 11:12:18 +0000

    layout change

--- src/app/3D-Model-AR/layout.js
+++ src/app/3D-Model-AR/layout.js
@@ -11,7 +11,7 @@ function Layout({ children }) {
         <meta charSet="utf-8" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, user-scalable=no, heigh=device-height"
         />
       </Head>
       <Script
