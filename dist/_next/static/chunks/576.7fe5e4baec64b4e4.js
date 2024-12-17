26e1ed4e367457bc4b7a363615ac42db2826dfbd
Ariya Bayat <ariyabayat778@gmail.com>
2024-12-17 14:19:05 +0000

    including build

---
+++ dist/_next/static/chunks/576.7fe5e4baec64b4e4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[576],{5576:(a,e,t)=>{t.r(e),t.d(e,{default:()=>o});var s=t(5155);let o=function(){return(0,s.jsx)("a-plane",{"follow-shadow":"#model-container",material:"shader:shadow",shadow:"cast:false;",rotation:"-90 0 0",position:"0 -0.5 -1",width:"10",height:"10"})}}}]);
